216e017e5d973df227a58bfce9c5574968fa777e
Adam Milazzo <adam@adammil.net>
2013-02-14 03:42:32 -0800

    Updated based on changes to utility library

--- RssFeed.cs
+++ RssFeed.cs
@@ -209,7 +209,7 @@ abstract class RssFeed : IDisposable
     try
     {
       responseStream = response.GetResponseStream();
-      return GetEncoding(response).GetString(responseStream.ReadAllBytes());
+      return GetEncoding(response).GetString(responseStream.ReadToEnd());
     }
     finally
     {
